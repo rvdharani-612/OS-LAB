77bbc7554b965d33802f7d8fa7a0fbb9f0410727
rvdharani-612 <dharanirv0612@gmail.com>
2026-09-07 00:29:30 +0530

    Add files via upload

---
+++ ex5/code_ex5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Hello from Child Process" | cat
+
+/*Alternative Shell Script
+#!/bin/bash
+(
+echo "Message from Child Process"
+) | while read msg
+do
+echo "Parent Received: $msg"
+done*/
