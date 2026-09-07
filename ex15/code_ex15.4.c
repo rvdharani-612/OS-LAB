08d68ac6e2a75ecb1dc30e625195847f13969b5c
rvdharani-612 <dharanirv0612@gmail.com>
2026-09-07 09:01:51 +0530

    Add files via upload

---
+++ ex15/code_ex15.4.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "SCAN Disk Scheduling"
+echo "Head moves in one direction and then reverses."
+#include<stdio.h>
+int main()
+{
+int head = 53;
+printf("C-SCAN Disk Scheduling\n");
+printf("Initial Head Position : %d\n",head);
+printf("Head moves in one direction.\n");
+printf("After reaching the end, it returns to the beginning.\n");
+return 0;
+}
